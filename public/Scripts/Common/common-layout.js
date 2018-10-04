3bae4c8f6ab4e7b78a929fc27b209619cfb2220a
Scott Becker <tsunamisukoto@hotmail.com>
2018-10-04 20:50:50 +1000

    sqlite3 stuff?

--- public/Scripts/Common/common-layout.js
+++ public/Scripts/Common/common-layout.js
@@ -1,5 +1,6 @@
 $(function () {
     $("#divHeader").load("header.html", function () {
+
         $('.navigation-bar-item[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
         $('.main-page-heading').click(function () {
             $(".navigation-items").toggleClass("shown");
